939e11c25e381d2fe4a4def6981971501e2f285c
AzerIsrafilzade <azisrafilzade@gmail.com>
2024-11-09 18:30:02 +0400

    Shopify-partly-comp-09-11-2024

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,42 @@ closeIcon.addEventListener("click", function() {
   
     mobileMenyu.classList.remove("aktiv")
 })
+
+
+
+const byShopifyDivi = document.querySelector("byShopify")
+
+
+window.addEventListener("load",getLocaldanMelumatlariGetir)
+
+async function getLocaldanMelumatlariGetir() {
+  const unvan = "assets/bySopify.json"
+
+
+  try {
+    const gelenCavab = await fetch (unvan)
+   
+    const mukafatlar = await gelenCavab.json()
+
+
+    byShopify.forEach(function(oneByShopify) {
+      byShopifyrDivi.innerHTML += `
+      <div class="swiper-slide text-center">
+<div class="card rounded-0">
+    <img src="${oneByShopify.byShopify_image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${oneByShopify.byShopify_name}</h5>
+      <p class="card-text">${oneByShopify.byShopify_context}</p>
+    </div>
+  </div>
+</div>
+      `
+  });
+
+}
+
+catch(err) {
+
+}
+  
+}
